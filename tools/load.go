93c6df326d5da3cb242df21ca4027f6dfe2e3d9d
Riley Douglas <rileydouglas@outlook.com>
2022-03-08 13:04:17 +1300

    import renamed to load

--- tools/load.go
+++ tools/load.go
@@ -2,10 +2,10 @@ package tools
 
 import "github.com/spf13/cobra"
 
-var importCmd = &cobra.Command{
-	Use:   "import [file-path]",
-	Short: "Import a list of horses",
-	Long: `file must be of *.txt and contain a single name for each line.
+var loadCmd = &cobra.Command{
+	Use:   "load [file-path]",
+	Short: "Loads a list of horses",
+	Long: `File must be of *.txt and contain a single name for each line.
 	The program will automatically supply and ID and MMR rating`,
 	Args: cobra.MinimumNArgs(1),
 	RunE: Run(),
