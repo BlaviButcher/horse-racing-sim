aae6744d713e713bac34c266d69cbf8a3171c8d8
Riley Douglas <rileydouglas@outlook.com>
2022-03-08 13:26:37 +1300

    getting records and creating db

---
+++ tools/horse_io.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/blavi/horse/horse/horsedb"
+)
+
+// getRecords opens and reads file into an Animal struct.
+// Returns an Animal array and error
+func (opts *loadCLIOpts) GetRecords() ([]*horsedb.Horse, error) {
+
+	horses := []*horsedb.Horse{}
+
+	file, err := os.Open(opts.InputFile)
+	if err != nil {
+		return nil, fmt.Errorf("opening file %w", err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		horses = append(horses, horsedb.NewHorse(scanner.Text()))
+	}
+
+	return horses, scanner.Err()
+}
--- tools/load.go
+++ tools/load.go
@@ -1,11 +1,22 @@
 package tools
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
 
-var loadCmd = &cobra.Command{
-	Use:   "load [file-path]",
+	"github.com/blavi/horse/horse/horsedb"
+	"github.com/spf13/cobra"
+)
+
+type loadCLIOpts struct {
+	DatabaseDir string
+	InputFile   string
+}
+
+var load = &cobra.Command{
+	Use:   "load [db-dir] [file-path]",
 	Short: "Loads a list of horses",
-	Long: `File must be of *.txt and contain a single name for each line.
+	Long: `db-dir is directory that database is found in
+	File must be of *.txt and contain a single name for each line.
 	The program will automatically supply and ID and MMR rating`,
 	Args: cobra.MinimumNArgs(1),
 	RunE: Run(),
@@ -13,6 +24,32 @@ var loadCmd = &cobra.Command{
 
 func Run() func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
+		opts := loadCLIOpts{}
+		opts.ParseArgs(args)
+
+		database, err := horsedb.OpenDatabase(opts.DatabaseDir)
+		if err != nil {
+			return fmt.Errorf("opening database: %w", err)
+		}
+		defer database.Close()
+
+		opts.GetRecords()
+
 		return nil
+
 	}
+}
+
+// ParseArgs checks the required args are present
+func (opts *loadCLIOpts) ParseArgs(args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("expected 2 arguments, got %d", len(args))
+	}
+	opts.DatabaseDir = args[0]
+	opts.InputFile = args[1]
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(load)
 }
